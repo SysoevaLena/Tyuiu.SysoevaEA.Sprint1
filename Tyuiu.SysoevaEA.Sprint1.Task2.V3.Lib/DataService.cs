21dd8cbbd59c53ffcd11bcc572975f58252463c1
SysoevaEA <sysoevalena413@gmail.com>
2026-09-24 23:51:10 +0500

    Task2

---
+++ Tyuiu.SysoevaEA.Sprint1.Task2.V3.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SysoevaEA.Sprint1.Task2.V3.Lib
+{
+    public class DataService : ISprint1Task2V3
+    {
+        public int ConvertHourToMin(int value)
+        {
+            return value * 60;
+        }
+    }
+}
---
+++ Tyuiu.SysoevaEA.Sprint1.Task2.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SysoevaEA.Sprint1.Task2.V3.Lib;
+
+namespace Tyuiu.SysoevaEA.Sprint1.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Сысоева Е. А. | ИБКСб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Сысоева Елена Андреевна | ИБКСб-26-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя количество       *");
+            Console.WriteLine("* времени в часах, переводит время в минуты и печатает его на экране.     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите количество часов X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.ConvertHourToMin(x));
+
+        }
+    }
+}
